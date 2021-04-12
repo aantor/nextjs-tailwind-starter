838a189db8852aa7b089c4c5e59129f57055a29f
aantor <antorinfo@gmail.com>
2021-04-12 10:22:58 +0600

    first commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <h1 className="text-purple-500 text-8xl">working...</h1>
+            </main>
+        </div>
+    );
+}
